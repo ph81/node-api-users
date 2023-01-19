686261f5dda06c8f39934b890ca7cbcc1b35038e
Edith M <paperhearts81@gmail.com>
2023-01-18 20:05:24 -0600

    chore: controller updated

--- userController.js
+++ userController.js
@@ -1,50 +1,50 @@
 // Import User Model
-const User = require('./userModel');
+const User = require("./userModel");
 
 //Handling index actions
-exports.index = function(req, res) {
-    User.find()
+exports.index = function (req, res) {
+  User.find()
     .exec()
-    .then(data => res.status(200).json(data));
-}
-
-// Create user
-exports.new = function (req, res) {
-    let user = new User();
-    user.userId = req.body.userId ? req.body.userId : user.userId;
-    user.name = req.body.name ? req.body.name : user.name;
-    user.email = req.body.email ? req.body.email : req.body.email;
-    user.avatar = req.body.avatar ? req.body.avatar : req.body.avatar;
-
-    //saving user and checking for errors
-    user.save(function (err) {
-        res.json({
-            message: 'New user created!',
-            status: 'ok',
-            data: user
-        });
-    });
+    .then((data) => res.status(200).json(data));
 };
 
+// Create book
+exports.new = function (req, res) {
+  let user = new User();
+  user.userId = req.body.userId ? req.body.userId : user.userId;
+  user.name = req.body.name ? req.body.name : user.name;
+  user.email = req.body.email ? req.body.email : user.email;
+  user.avatar = req.body.avatar ? req.body.avatar : user.avatar;
 
-// view user info
+  //saving user and checking for errors
+  user.save(function (err) {
+    res.json({
+      message: "New user created!",
+      status: "ok",
+      data: user,
+    });
+  });
+};
+
+// Handle view book info
 exports.view = async function (req, res) {
-    try {
-      const data = await User.findOne({userId: req.params.userId});
-      res.json(data);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+  try {
+    const data = await User.findOne({userId: req.params.userId});
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-// update user info
+// Handle update note info
 exports.update = function (req, res) {
     User.findOne({userId: req.params.userId}, function (err, user) {
         if (err)
             res.send(err);
         user.name = req.body.name ? req.body.name : user.name;
-        user.email = req.body.email ? req.body.email : req.body.email;
-        user.avatar = req.body.avatar ? req.body.avatar : req.body.avatar;
+        user.email = req.body.email ? req.body.email : user.email;
+        user.avatar = req.body.avatar ? req.body.avatar : user.avatar;
+ 
 
         // save the user and check for errors
         user.save(function (err) {
@@ -59,18 +59,19 @@ exports.update = function (req, res) {
     });
 };
 
-
-// delete user
+// Handle delete user
 exports.delete = function (req, res) {
-    User.deleteOne(
-        {userId: req.params.userId},
-      function (err, user) {
-        if (err) res.send(err);
-  
-        res.json({
-          status: "ok",
-          message: "user deleted",
-        });
-      }
-    );
-  };
+  User.deleteOne(
+    {
+      userId: req.params.userId,
+    },
+    function (err, user) {
+      if (err) res.send(err);
+
+      res.json({
+        status: "ok",
+        message: "user deleted",
+      });
+    }
+  );
+};
